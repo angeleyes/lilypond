55c94dbd4105f42cb0d650431d02a2cf393d5b70
fred <fred>
1998-09-29 11:55:20 +0000

    lilypond-1.0.18

--- buildscripts/ps-to-gifs.sh
+++ buildscripts/ps-to-gifs.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-# ps-to-gifs, convert PS to multiple gifs
-  
+# ps-to-gifs, convert PS to multiple gifs or other bitmaps
+
 usage()
 {
     cat <<EOF
+Convert PS to multiple gifs or other bitmaps
 Usage: ps-to-gifs.sh [OPTION]... [FILE]
 Options:
   -h, --help         this help
   -c, --crop         crop output
   -o, --output=NAME  set output base
+  -p, --png          convert to png
+  -s, --size=SIZE    set papersize
   -t, --transparent  change white to transparent
 EOF
 }
@@ -18,6 +21,8 @@ if [ $# -lt 1 ]; then
     exit 2;
 fi
 CROP=cat
+GIF=gif
+PNMTOGIF=ppmtogif
 
 while [ $# -gt 0 ]; do
 opt=$1
@@ -37,6 +42,15 @@ shift
         ;;
     --o*=*) OUTFILE=`echo $opt | sed -e s/"^.*="//`
         ;;
+    -p|--p*)
+	GIF=png
+	PNMTOGIF=pnmtopng
+	;;
+    -s) SIZE="-sPAPERSIZE=$2"; shift
+        ;;
+    --s*=*)
+        SIZE="-sPAPERSIZE=`echo $opt | sed -e s/"^.*="//`"
+	;;
     -*)
         echo "ps-to-gifs: unknown option: \`$opt'"
 	exit 1
@@ -54,24 +68,24 @@ fi
 if [ "x$OUTFILE" = "x" ]; then
 	BASE=`dirname $FILE`/`basename $FILE .ps`
 else
-	BASE=`dirname $OUTFILE`/`basename $OUTFILE .gif`
+	BASE=`dirname $OUTFILE`/`basename $OUTFILE .$GIF`
 fi
 
 # urg, pipe breaks
-rm -f $BASE{.ppm,.gif} $BASE-page*{.ppm,.gif}
+rm -f $BASE{.ppm,.$GIF} $BASE-page*{.ppm,.$GIF}
 
 # generate the pixmap at twice the size, then rescale (for antialiasing)
-cat $FILE | gs -sDEVICE=ppmraw -sOutputFile="$BASE-page%d.ppm" -r200 -dNOPAUSE - -c quit $FILE
+cat $FILE | gs -sDEVICE=ppmraw $SIZE -sOutputFile="$BASE-page%d.ppm" -r180 -dNOPAUSE - -c quit $FILE
 # quant is soo slow
-# cat $PPMFILE | ppmquant 2 | pnmscale 0.3333 | pnmcrop | ppmtogif $color > $OUTFILE
+# cat $PPMFILE | ppmquant 2 | pnmscale 0.3333 | pnmcrop | $PNMTOGIF $color > $OUTFILE
 PPMS=`ls $BASE*ppm`
 for i in $PPMS; do
-    o=`dirname $i`/`basename $i .ppm`.gif
-    cat $i | pnmscale 0.5 | $CROP | ppmtogif $color > $o
+    o=`dirname $i`/`basename $i .ppm`.$GIF
+    cat $i | pnmscale 0.5 | $CROP | $PNMTOGIF $color > $o
     rm $i
 done
 
 if [ "x$OUTFILE" != "x" ]; then
-	mv $BASE-page1.gif $BASE.gif
+	mv $BASE-page1.$GIF $BASE.$GIF
 fi
 
