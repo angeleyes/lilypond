c4d6bd6a20bf5efda01155d9852c24c5f8c2a9c6
fred <fred>
2000-07-17 10:44:57 +0000

    lilypond-1.4.5

---
+++ flower/include/input.hh
@@ -0,0 +1,42 @@
+/*
+  input.hh -- declare Input
+
+  source file of the LilyPond music typesetter
+
+  (c)  1997--2000 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+*/
+
+
+#ifndef INPUT_HH
+#define INPUT_HH
+
+#include "flower-proto.hh"
+
+/**
+  Base class for anything that records its poisition in the parse file.
+ */
+class Input {
+  char const *defined_ch_C_ ;
+  Source_file * source_file_l_;
+public:
+    
+  void warning (String) const; // should use member func?
+  void non_fatal_error (String) const;
+  void error (String) const;
+  void message (String) const;
+  void set_spot (Input const &);
+  Input spot () const;
+  String location_str () const;
+  String line_number_str () const;
+
+
+  String file_str ()const;
+  int line_number ()const;
+  int column_number ()const;
+
+  
+  Input (Source_file*, char const*);
+  Input ();
+};
+
+#endif // INPUT_HH
