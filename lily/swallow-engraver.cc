7842a249c5b8f719d8823db9829e28ada00f7239
fred <fred>
2001-02-27 13:56:29 +0000

    lilypond-1.4.5

--- lily/swallow-engraver.cc
+++ lily/swallow-engraver.cc
@@ -3,17 +3,27 @@
 
   source file of the GNU LilyPond music typesetter
 
-  (c)  1997--1999 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+  (c)  1997--2001 Han-Wen Nienhuys <hanwen@cs.uu.nl>
 */
-#include "swallow-engraver.hh"
 
+#include "engraver.hh"
 
+/**
+  This engraver swallows everything given to it silently. The purpose of
+  this is to prevent spurious "request junked" warnings.
+ */
+class Swallow_engraver : public Engraver
+{
+public:
+  VIRTUAL_COPY_CONS (Translator);
+protected:
+  bool try_music (Music*) ;
+};
 
-ADD_THIS_TRANSLATOR(Swallow_engraver);
-
+ADD_THIS_TRANSLATOR (Swallow_engraver);
 
 bool
-Swallow_engraver::do_try_music (Music*) 
+Swallow_engraver::try_music (Music*) 
 {
   return true;
 }
