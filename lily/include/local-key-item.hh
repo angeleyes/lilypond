c0425c6a6ba11c770532a183aaea1bcbd5c7a699
fred <fred>
2001-02-27 17:35:55 +0000

    lilypond-1.4.5

--- lily/include/local-key-item.hh
+++ lily/include/local-key-item.hh
@@ -1,54 +1,27 @@
 /*
   local-key-item.hh -- part of GNU LilyPond
 
-  (c) 1996--1999 Han-Wen Nienhuys
+  (c) 1996--2001 Han-Wen Nienhuys
 */
 
 #ifndef LOCALKEYITEM_HH
 #define LOCALKEYITEM_HH
-#include "item.hh"
+
+
 #include "array.hh"
-#include "musical-pitch.hh"
-#include "staff-symbol-referencer.hh"
-#include "note-head-side.hh"
+#include "pitch.hh"
 
-struct Local_key_cautionary_tuple
+class Local_key_item
 {
-  Musical_pitch pitch_;
-  bool cautionary_b_;
-
-  Local_key_cautionary_tuple ()
-    {
-      cautionary_b_ = false;
-    }
-  static int compare (Local_key_cautionary_tuple const&s1, Local_key_cautionary_tuple const&s2)
-    {
-      return Musical_pitch::compare (s1.pitch_, s2.pitch_);
-    }
-};
-
-/**
-  Accidentals which can be different for each octave.
-
-  TODO:
-  update item if Items are removed
-
-  TODO
-  
-  figure out private/public
-  
- */
-class Local_key_item : public Note_head_side, public Staff_symbol_referencer {
-  Array<Local_key_cautionary_tuple> accidental_arr_;
-
+  static Molecule parenthesize (Grob*me, Molecule) ;
 public:
-  int c0_position_i_;
-  Local_key_item ();
-  void add_pitch (Musical_pitch, bool cautionary);
-protected:
-  virtual void do_pre_processing();
-  virtual void do_substitute_element_pointer (Score_element*,Score_element*);
-  virtual Molecule* do_brew_molecule_p() const;
+  DECLARE_SCHEME_CALLBACK (after_line_breaking, (SCM));
+  DECLARE_SCHEME_CALLBACK (brew_molecule, (SCM));
+  static void add_pitch (Grob*me, Pitch, bool cautionary, bool natural, Grob *tie_break_cautionary);
+  static bool has_interface (Grob*);
+  static void set_interface (Grob*);  
 };
+
+
 #endif // LOCALKEYITEM_HH
 
