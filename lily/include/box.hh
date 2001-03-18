433c401e0aa0ef9d1e0ea89c7b6e424b71003b33
fred <fred>
2001-03-18 16:14:26 +0000

    lilypond-1.4.5

--- lily/include/box.hh
+++ lily/include/box.hh
@@ -5,28 +5,30 @@
 #ifndef BOXES_HH
 #define BOXES_HH
 
-#include "fproto.hh"
+#include "flower-proto.hh"
 #include "real.hh"
 #include "interval.hh"
 #include "offset.hh"
 #include "axes.hh"
 
 
-struct Box {
+struct Box
+{
   Interval interval_a_[NO_AXES];
     
-  Interval &x() {return interval_a_[X_AXIS]; }
-  Interval &y(){ return interval_a_[Y_AXIS]; }
-  Interval x() const{ return interval_a_[X_AXIS]; }
-  Interval y() const{return interval_a_[Y_AXIS]; }
-  Interval operator[](Axis a) const;
+  Interval &x () {return interval_a_[X_AXIS]; }
+  Interval &y (){ return interval_a_[Y_AXIS]; }
+  Interval x () const{ return interval_a_[X_AXIS]; }
+  Interval y () const{return interval_a_[Y_AXIS]; }
+  Interval operator[] (Axis a) const;
   Interval &operator[] (Axis a);
     
   void translate (Offset o);
   /// smallest box enclosing #b#
   void set_empty ();
+  void scale (Real r);
   void unite (Box b);
-  Box();
+  Box ();
   Box (Interval ix, Interval iy);
 };
 
