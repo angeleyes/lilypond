f3dee58e978be0a5c632998697ee94c0a506899f
fred <fred>
2001-04-23 23:23:32 +0000

    lilypond-1.4.5

--- lily/performer-group-performer.cc
+++ lily/performer-group-performer.cc
@@ -3,16 +3,17 @@
 
   source file of the GNU LilyPond music typesetter
 
-  (c) 1996,  1997--1999 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+  (c) 1996--2001 Han-Wen Nienhuys <hanwen@cs.uu.nl>
                Jan Nieuwenhuizen <janneke@gnu.org>
  */
 
 #include "performer-group-performer.hh"
 
+#include "audio-element.hh"
 #include "debug.hh"
 
 
-ADD_THIS_TRANSLATOR(Performer_group_performer);
+ADD_THIS_TRANSLATOR (Performer_group_performer);
 
 
 void
@@ -22,48 +23,62 @@ Performer_group_performer::announce_element (Audio_element_info info)
   Performer::announce_element (info);
 }
 
+
+
 void
-Performer_group_performer::do_announces()
+Performer_group_performer::create_audio_elements ()
 {
-  Link_array<Translator_group> groups = group_l_arr ();
-  for (int i=0; i < groups.size(); i++) 
+  for (SCM p = simple_trans_list_; gh_pair_p (p); p = gh_cdr (p))
     {
-      Performer_group_performer * group = dynamic_cast<Performer_group_performer*> (groups[i]);
-      if (group)
-	{
-	  group->do_announces();
-	}
-    }
-  
-  Request dummy_req;
-
-  Link_array<Translator> nongroups = nongroup_l_arr ();
-  while (announce_info_arr_.size ())
-    {
-      for (int j =0; j < announce_info_arr_.size(); j++)
-	{
-	  Audio_element_info info = announce_info_arr_[j];
-	  
-	  if (!info.req_l_)
-	    info.req_l_ = &dummy_req;
-	  for (int i=0; i < nongroups.size(); i++) 
-	    {	// Is this good enough?
-	      Performer * eng = dynamic_cast<Performer*> (nongroups[i]);
-	      // urg, huh? core dump?
-	      //if (eng && eng!= info.origin_trans_l_arr_[0])
-	      if (eng && info.origin_trans_l_arr_.size ()
-		  && eng!= info.origin_trans_l_arr_[0])
-		eng->acknowledge_element (info);
-	    }
-	}
-      announce_info_arr_.clear ();
-      for (int i=0; i < nongroups.size(); i++)
-	{
-	  Performer * eng = dynamic_cast<Performer*> (nongroups[i]);
-	  if (eng)
-	    eng->process_acknowledged ();
-	}
-
+      Translator * t = unsmob_translator (gh_car (p));
+      Performer * eng = dynamic_cast<Performer*> (t);
+      if (eng)
+	eng->create_audio_elements ();
     }
 }
 
+void
+Performer_group_performer::acknowledge_audio_elements ()
+{
+  for (int j =0; j < announce_info_arr_.size (); j++)
+    {
+      Audio_element_info info = announce_info_arr_[j];
+
+      for (SCM p = simple_trans_list_; gh_pair_p (p); p = gh_cdr (p))
+	{
+	  Translator * t = unsmob_translator (gh_car (p));
+	  Performer * eng = dynamic_cast<Performer*> (t);
+	  if (eng && eng!= info.origin_trans_l_)
+	    eng->acknowledge_audio_element (info);
+	}
+    }
+}
+
+void
+Performer_group_performer::do_announces ()
+{
+  for (SCM p = trans_group_list_; gh_pair_p (p); p =gh_cdr (p))
+    {
+      Translator * t = unsmob_translator (gh_car (p));
+      dynamic_cast<Performer_group_performer*> (t)->do_announces ();
+    }
+
+  
+  create_audio_elements ();
+    
+  while (announce_info_arr_.size ())
+    {
+      acknowledge_audio_elements ();
+      announce_info_arr_.clear ();
+      create_audio_elements ();
+    }
+
+  if (announce_info_arr_.size ())
+    {
+#if 0  //printf?  -> include <stdio.h>     
+      printf ("do_announces: elt: %s\n",
+	      classname (announce_info_arr_[0].elem_l_));
+#endif      
+      announce_info_arr_.clear ();
+    }
+}
