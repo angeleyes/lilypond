a9afc5082dd1cd74b854125dc9fad2d14289f42e
fred <fred>
2001-05-02 21:56:40 +0000

    lilypond-1.4.5

--- lily/include/span-bar.hh
+++ lily/include/span-bar.hh
@@ -3,43 +3,35 @@
 
   source file of the GNU LilyPond music typesetter
 
-  (c)  1997--1999 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+  (c)  1997--2001 Han-Wen Nienhuys <hanwen@cs.uu.nl>
 */
 
 
 #ifndef SPAN_BAR_HH
 #define SPAN_BAR_HH
 
-#include "bar.hh"
+#include "lily-guile.hh"
+#include "lily-proto.hh"
 
 /**
    This is a barline that is spanned across other bar lines.  This is
    the implementation of the long barlines that occur in orchestral
    score and other multi-staff music.
-
-   TODO: Is this usable for other stuff besides barlines? We only have
-   to span a Score_element.  Perhaps this can be used for large time
-   sigs?
 */
-class Span_bar : public Bar
+class Span_bar
 {
-  Link_array<Score_element> spanning_l_arr_;
-  Interval get_spanned_interval () const;
 public:
-  Span_bar();
-    
-  VIRTUAL_COPY_CONS(Score_element);
-  void add_bar (Score_element*);
-  void set_align (Align_element *);
-protected:
-  void evaluate_empty ();
-
-  virtual Interval do_width() const;
-  virtual void do_pre_processing();
-  virtual void do_post_processing();
-  virtual Interval do_height () const;
-  virtual void do_substitute_element_pointer (Score_element*,Score_element*);
-  virtual Molecule * do_brew_molecule_p() const;
+  static void set_interface (Grob*);
+  static bool has_interface (Grob*);  
+  static Interval get_spanned_interval (Grob*);
+  static void add_bar (Grob*,Grob*);
+  static void evaluate_glyph (Grob*);
+  static void evaluate_empty (Grob*);
+  DECLARE_SCHEME_CALLBACK (width_callback, (SCM smob, SCM axis));
+  DECLARE_SCHEME_CALLBACK (get_bar_size, (SCM ));
+  DECLARE_SCHEME_CALLBACK (brew_molecule, (SCM ));
+  DECLARE_SCHEME_CALLBACK (before_line_breaking, (SCM ));
+  DECLARE_SCHEME_CALLBACK (center_on_spanned_callback, (SCM element, SCM axis));
 };
 
 #endif // SPAN_BAR_HH
