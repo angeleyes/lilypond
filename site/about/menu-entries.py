e90cb991dd8bbef2e0ecaedb20f94ddb2793e6be
hanwen <hanwen>
2003-08-04 09:34:36 +0000

    move to switch/howto.html

--- site/about/menu-entries.py
+++ site/about/menu-entries.py
@@ -2,8 +2,7 @@
 [('news.html', "News"),
  ('features.html', 'Features'),
  ('faq.html', 'FAQ'),
- ('screenshots.html', 'Screenshots'),
  ('pubs.html', 'Publications'),
  ('donate.html', 'Donate'),
- 
+ ('thanks.html', "Acknowledgements"),
 ]
