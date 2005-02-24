f1e0f0d704715a18275732d6b3ee75f6c6477a47
janneke <janneke>
2005-02-24 07:22:34 +0000

    Add documentation-adding.

--- site/devel/participating/menu-entries.py
+++ site/devel/participating/menu-entries.py
@@ -1,5 +1,6 @@
 [
  ("call-for-help.html", _ ("Help wanted!")),
+ ("documentation-adding.html", _ ("Documentation")),
  ("localization.html", _ ("Localization")),
  ("standards.html", _ ("Standards")),
  ]
