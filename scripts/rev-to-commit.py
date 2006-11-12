96d77c213c557a5e014d6c4285352eb982fd7faa
Jan Nieuwenhuizen <janneke@gnu.org>
2006-11-12 20:41:32 +0100

    Add CVS revision to GIT commit script.

---
+++ scripts/rev-to-commit.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+def read_pipe (command):
+    import os
+    pipe = os.popen (command)
+    output = pipe.read ()
+    if pipe.close ():
+        print "pipe failed: %(command)s" % locals ()
+        raise 'SystemFailed'
+    return output
+
+def file_sub (file, frm, to):
+    s = open (file).read ()
+    open (file, 'w').write (s.replace (frm, to))
+    
+for file in read_pipe ('''git grep -l "CVS Revision: [0-9]"''').split ('\n'):
+    revision = ''
+    if not file or file.find ('big-page') >= 0:
+        continue
+    print file
+    hunt = '''git grep -h "CVS Revision: [0-9]" %(revision)s -- %(file)s'''
+    cvs_revision_string = read_pipe (hunt % locals ()).strip ()
+    s = cvs_revision_string
+    r = 0
+    while s == cvs_revision_string:
+        r += 1
+        revision = 'HEAD~%d' % r
+        s = read_pipe (hunt % locals ())
+    rev = 'HEAD~%d' % (r - 1)
+    commit_cmd = '''git log %(rev)s --max-count=1|grep commit'''
+    commit = read_pipe (commit_cmd % locals ()).strip ()
+    commit_string = commit.replace ('commit ', 'Translation of GIT commit: ')
+    file_sub (file, cvs_revision_string, commit_string)
+    print cvs_revision_string, '->', commit_string
