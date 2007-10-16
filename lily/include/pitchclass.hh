e5f698927080f453b4d3bb56bddd83c2e95aa43e
Rune Zedeler <rune@zedeler.dk>
2007-10-16 15:10:41 +0200

    midt i at ændre pitch og tilføje pitchclass

---
+++ lily/include/pitchclass.hh
@@ -0,0 +1,75 @@
+#ifndef MUSICAL_PITCHCLASS_HH
+#define MUSICAL_PITCHCLASS_HH
+
+#include "lily-proto.hh"
+#include "smobs.hh"
+#include "rational.hh"
+
+
+class Pitchclass
+{
+private:
+  int notename_;
+  Rational alteration_;
+  Scale *scale_;
+
+  void transpose (Pitchclass);
+  virtual void normalize ();
+
+public:
+  int get_notename () const;
+  Rational get_alteration () const;
+
+  Pitchclass (int notename, Rational accidental);
+  Pitchclass (int notename);
+  Pitchclass ();
+
+  virtual Pitchclass transposed (Pitchclass) const;
+
+  // not virtual. Formal parameter types decide which comparation to do
+  static int compare (Pitchclass const &, Pitchclass const &);
+
+  //virtual Rational tone_pitch () const;
+  //int rounded_semitone_pitch () const;
+  //int rounded_quartertone_pitch () const;
+  virtual Pitchclass negated () const;
+  string to_string () const;
+
+  DECLARE_SCHEME_CALLBACK (less_p, (SCM a, SCM b));
+  DECLARE_SIMPLE_SMOBS (Pitchclass);
+};
+
+
+enum {
+  DOUBLE_FLAT = -4,
+  THREE_Q_FLAT,
+  FLAT,
+  SEMI_FLAT,
+  NATURAL,
+  SEMI_SHARP,
+  SHARP,
+  THREE_Q_SHARP,
+  DOUBLE_SHARP,
+};
+
+extern Rational  DOUBLE_FLAT_ALTERATION;
+extern Rational  THREE_Q_FLAT_ALTERATION;
+extern Rational  FLAT_ALTERATION;
+extern Rational  SEMI_FLAT_ALTERATION;
+extern Rational  NATURAL_ALTERATION;
+extern Rational  SEMI_SHARP_ALTERATION;
+extern Rational  SHARP_ALTERATION;
+extern Rational  THREE_Q_SHARP_ALTERATION;
+extern Rational  DOUBLE_SHARP_ALTERATION;
+
+//SCM ly_pitch_diff (SCM pitch, SCM root);
+//SCM ly_pitch_transpose (SCM p, SCM delta);
+DECLARE_UNSMOB (Pitchclass, pitchclass);
+
+INSTANTIATE_COMPARE (Pitchclass, Pitchclass::compare);
+
+extern SCM pitchclass_less_proc;
+Pitchclass pitchclass_interval (Pitchclass const &from, Pitchclass const &to);
+
+#endif /* MUSICAL_PITCH_HH */
+
---
+++ lily/pitchclass.cc
@@ -0,0 +1,319 @@
+/*
+  pitchclass.cc -- implement Pitch
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1998--2007 Han-Wen Nienhuys <hanwen@xs4all.nl>
+  2007 Rune Zedeler
+*/
+
+#include "pitchclass.hh"
+
+#include "main.hh"
+#include "scale.hh"
+#include "string-convert.hh"
+#include "warn.hh"
+
+#include "ly-smobs.icc"
+
+
+Pitchclass::Pitchclass (int n, Rational a)
+{
+  notename_ = n;
+  alteration_ = a;
+  scale_ = default_global_scale; 
+  normalize ();
+}
+
+Pitchclass::Pitchclass ()
+{
+  notename_ = 0;
+  scale_ = default_global_scale; 
+}
+
+int
+Pitchclass::compare (Pitchclass const &m1, Pitchclass const &m2)
+{
+  int n = m1.notename_ - m2.notename_;
+  Rational a = m1.alteration_ - m2.alteration_;
+
+  if (n)
+    return n;
+  if (a)
+    return a;
+  
+  return 0;
+}
+
+int
+Pitch::steps () const
+{
+  return notename_ + octave_ * scale_->step_tones_.size ();
+}
+
+Rational
+Pitch::tone_pitch () const
+{
+  int o = octave_;
+  int n = notename_;
+  while (n < 0)
+    {
+      n += scale_->step_tones_.size ();
+      o--;
+    }
+
+  /*
+    we're effictively hardcoding the octave to 6 whole-tones,
+    which is as arbitrary as coding it to 1200 cents
+  */
+  Rational tones ((o + n / scale_->step_tones_.size ()) * 6, 1);
+  tones += scale_->step_tones_[n % scale_->step_tones_.size ()];
+
+  tones += alteration_;
+  
+  return tones;
+}
+
+/* Calculate pitch height in 12th octave steps.  Don't assume
+   normalized pitch as this function is used to normalize the pitch.  */
+int
+Pitch::rounded_semitone_pitch () const
+{
+  return int (round (double (tone_pitch () * Rational (2))));
+}
+
+int
+Pitch::rounded_quartertone_pitch () const
+{
+  return int (round (double (tone_pitch () * Rational (4))));
+}
+
+void
+Pitch::normalize ()
+{
+  Rational pitch = tone_pitch ();
+  while (notename_ >= (int) scale_->step_tones_.size ())
+    {
+      notename_ -= scale_->step_tones_.size ();
+      octave_++;
+      // why? isn't this always 0? -rz
+      alteration_ -= tone_pitch () - pitch;
+    }
+  while (notename_ < 0)
+    {
+      notename_ += scale_->step_tones_.size ();
+      octave_--;
+      // do -rz
+      alteration_ -= tone_pitch () - pitch;
+    }
+
+  while (alteration_ > Rational (1))
+    {
+      // code duplication. Remove this and move the two above while-loops below this one: -rz
+      if (notename_ == int (scale_->step_tones_.size ()))
+	{
+	  notename_ = 0;
+	  octave_++;
+	}
+      else
+	notename_++;
+
+      // isn't the below the same as alteration_ = pitch - tone_pitch() -rz
+      alteration_ = Rational (0);
+      alteration_ -= tone_pitch () - pitch;
+    }
+  while (alteration_ < Rational (-1))
+    {
+      // do -rz
+      if (notename_ == 0)
+	{
+	  notename_ = scale_->step_tones_.size ();
+	  octave_--;
+	}
+      else
+	notename_--;
+
+      // do -rz
+      alteration_ = 0;
+      alteration_ -= tone_pitch () - pitch;
+    }
+}
+
+void
+Pitch::transpose (Pitch delta)
+{
+  Rational new_alter = tone_pitch () + delta.tone_pitch ();
+
+  octave_ += delta.octave_;
+  notename_ += delta.notename_;
+  alteration_ += new_alter - tone_pitch ();
+
+  normalize ();
+}
+
+Pitch
+pitch_interval (Pitch const &from, Pitch const &to)
+{
+  Rational sound = to.tone_pitch () - from.tone_pitch ();
+  Pitch pt (to.get_octave () - from.get_octave (),
+	    to.get_notename () - from.get_notename (),
+
+	    to.get_alteration () - from.get_alteration ());
+
+  return pt.transposed (Pitch (0, 0, sound - pt.tone_pitch ()));
+}
+
+/* FIXME
+   Merge with *pitch->text* funcs in chord-name.scm  */
+char const *accname[] = {"eses", "eseh", "es", "eh", "",
+			 "ih", "is", "isih", "isis"};
+
+string
+Pitch::to_string () const
+{
+  int n = (notename_ + 2) % scale_->step_tones_.size ();
+  string s = ::to_string (char (n + 'a'));
+  Rational qtones = alteration_ * Rational (4,1);
+  int qt = int (rint (Real (qtones)));
+      
+  s += string (accname[qt + 4]);
+  if (octave_ >= 0)
+    {
+      int o = octave_ + 1;
+      while (o--)
+	s += "'";
+    }
+  else if (octave_ < 0)
+    {
+      int o = (-octave_) - 1;
+      while (o--)
+	s += ::to_string (',');
+    }
+
+  return s;
+}
+
+/* Change me to relative, counting from last pitch p
+   return copy of resulting pitch.  */
+Pitch
+Pitch::to_relative_octave (Pitch p) const
+{
+  /* account for c' = octave 1 iso. 0 4 */
+  int oct_mod = octave_ + 1;
+  Pitch up_pitch (p);
+  Pitch down_pitch (p);
+
+  up_pitch.alteration_ = alteration_;
+  down_pitch.alteration_ = alteration_;
+
+  Pitch n = *this;
+  up_pitch.up_to (notename_);
+  down_pitch.down_to (notename_);
+
+  int h = p.steps ();
+  if (abs (up_pitch.steps () - h) < abs (down_pitch.steps () - h))
+    n = up_pitch;
+  else
+    n = down_pitch;
+
+  n.octave_ += oct_mod;
+  return n;
+}
+
+void
+Pitch::up_to (int notename)
+{
+  if (notename_ > notename)
+    octave_++;
+  notename_ = notename;
+}
+
+void
+Pitch::down_to (int notename)
+{
+  if (notename_ < notename)
+    octave_--;
+  notename_ = notename;
+}
+
+IMPLEMENT_TYPE_P (Pitch, "ly:pitch?");
+SCM
+Pitch::mark_smob (SCM x)
+{
+  Pitch *p = (Pitch*) SCM_CELL_WORD_1 (x);
+  return p->scale_->self_scm ();
+}
+
+IMPLEMENT_SIMPLE_SMOBS (Pitch);
+int
+Pitch::print_smob (SCM s, SCM port, scm_print_state *)
+{
+  Pitch *r = (Pitch *) SCM_CELL_WORD_1 (s);
+  scm_puts ("#<Pitch ", port);
+  scm_display (ly_string2scm (r->to_string ()), port);
+  scm_puts (" >", port);
+  return 1;
+}
+
+SCM
+Pitch::equal_p (SCM a, SCM b)
+{
+  Pitch *p = (Pitch *) SCM_CELL_WORD_1 (a);
+  Pitch *q = (Pitch *) SCM_CELL_WORD_1 (b);
+
+  bool eq = p->notename_ == q->notename_
+    && p->octave_ == q->octave_
+    && p->alteration_ == q->alteration_;
+
+  return eq ? SCM_BOOL_T : SCM_BOOL_F;
+}
+
+MAKE_SCHEME_CALLBACK (Pitch, less_p, 2);
+SCM
+Pitch::less_p (SCM p1, SCM p2)
+{
+  Pitch *a = unsmob_pitch (p1);
+  Pitch *b = unsmob_pitch (p2);
+
+  if (compare (*a, *b) < 0)
+    return SCM_BOOL_T;
+  else
+    return SCM_BOOL_F;
+}
+
+int
+Pitch::get_octave () const
+{
+  return octave_;
+}
+
+int
+Pitch::get_notename () const
+{
+  return notename_;
+}
+
+Rational
+Pitch::get_alteration () const
+{
+  return alteration_;
+}
+
+Pitch
+Pitch::transposed (Pitch d) const
+{
+  Pitch p = *this;
+  p.transpose (d);
+  return p;
+}
+
+Rational NATURAL_ALTERATION (0);
+Rational FLAT_ALTERATION (-1, 2);
+Rational DOUBLE_FLAT_ALTERATION (-1);
+Rational SHARP_ALTERATION (1, 2);
+
+Pitch
+Pitch::negated () const
+{
+  return pitch_interval (*this, Pitch ());
+}
