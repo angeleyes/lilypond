322c45a8b4d41d2942ffd9bf37ec76cc6b5da347
Graham Percival <graham@percival-music.ca>
2009-07-27 17:57:14 -0700

    Web: more top-level files for the "web repo".

---
+++ lilypond-web-git-repo/scripts/apply-versions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+print "This script will write the current STABLE-VERSION and DEVEL-VERSION links to the website output.  It's the successor to format-page.py."
+
+print "It also writes the download links."
+
+print "And also whatever else needs to be done.  :)"
+
