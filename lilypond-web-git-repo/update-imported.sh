148202f8898f7f768e5ed2c6edc7f0cb16a64279
Graham Percival <graham@percival-music.ca>
2009-07-27 17:33:19 -0700

    Web: add proposal of separate web repo.

---
+++ lilypond-web-git-repo/update-imported.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+### FIXME: figure out how to get the lastest such dir, not a
+###        specific commit.  Or get individual files
+### TODO: change url if/when it's merged with master
+
+wget -O snapshot.tar.gz "http://git.savannah.gnu.org/gitweb/?p=lilypond.git;a=snapshot;h=6625e6514c82cd283100c055b8f2ffa9765efbc6;sf=tgz"
+
+### unpack
+tar -xzf snapshot.tar.gz
+
+### simulate merged stuff  (these files won't be master)
+rm lilypond/examples/*.png
+rm lilypond/SConstruct
+rm -rf lilypond/site_scons
+rm lilypond/news-to-texi.py
+
+### overwrite imported
+rm -rf imported
+mv lilypond/ imported/
+
+
+
